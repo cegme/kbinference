9250cf456af8acfff9b419c0f3c0e7e10bdaf4b5
Christan Grant <christangrant@gmail.com>
2015-07-06 23:53:03 -0400

    Added a new test file and the large, clean knowledge base. Need to do some magic to make the system able to load the whole graph

---
+++ src/test/groovy/com/cegme/kbinference/graph/LargeGraphTest.groovy
@@ -0,0 +1,45 @@
+package com.cegme.kbinference.graph
+
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph
+import com.tinkerpop.gremlin.groovy.Gremlin
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+public class LargeGraphTest {
+    TinkerGraph graph
+
+    @BeforeClass
+    static void beforeClass(){
+        Gremlin.load()
+    }
+
+    @Before
+    void setUp() throws Exception {
+        graph = ReverbGraph.loadGraph('/reverb_clueweb_tuples-1.1.triples.clean.csv')
+    }
+
+    @Test
+    void testGraphLoaded(){
+        def vertexCnt = graph.vertices.size()
+        assertEquals('All vertices were loaded', 2220591, vertexCnt)
+    }
+
+    @Test
+    void testQueries(){
+        def chargerVertexCnt = graph.V('noun', 'the Chargers').count()
+        assertEquals('Able to query by vertex property', 137, chargerVertexCnt)
+
+        def whoGotJoeMontana = graph.V('noun', 'Joe Montana').inE('traded for').outV.noun.next()
+        assertEquals('Able to query & traverse the graph', 'the Chiefs', whoGotJoeMontana)
+    }
+
+
+    @After
+    void tearDown() throws Exception {
+        graph.shutdown()
+    }
+}
