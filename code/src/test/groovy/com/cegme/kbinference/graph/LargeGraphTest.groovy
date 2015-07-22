b75be3d2888d91bf8eaf4afa1500cd74f09201ff
Craig W <cwash.dev@gmail.com>
2015-07-21 23:23:35 -0400

    Test fix + adding a mvn property to control graph creation

--- code/src/test/groovy/com/cegme/kbinference/graph/LargeGraphTest.groovy
+++ code/src/test/groovy/com/cegme/kbinference/graph/LargeGraphTest.groovy
@@ -1,11 +1,10 @@
 package com.cegme.kbinference.graph
 
 import com.thinkaurelius.titan.core.TitanFactory
-import com.thinkaurelius.titan.core.TitanGraph
-import com.tinkerpop.blueprints.Graph
 import com.tinkerpop.blueprints.TransactionalGraph
 import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.gremlin.groovy.Gremlin
+import groovy.util.logging.Slf4j
 import org.apache.commons.configuration.BaseConfiguration
 import org.apache.commons.io.FileUtils
 import org.junit.AfterClass
@@ -14,60 +13,61 @@ import org.junit.Test
 
 import static org.junit.Assert.assertEquals
 
+@Slf4j
 public class LargeGraphTest {
     private static Properties props = new Properties()
     static TransactionalGraph graph
 
     @BeforeClass
-    static void initDb(){
-       
-
-        Scanner userInput = new Scanner(System.in)
-        System.out.print("Do you want to load a new graph (y/n)?\n>> ")
-        char c = userInput.nextChar().toLowerCase()
-
+    static void initDb() {
         Gremlin.load()
 
         props.load(GraphTest.class.getResourceAsStream('/test.properties'))
+        boolean createGraph = props.getProperty('populate.graph', 'false').toBoolean()
         def path = props.getProperty('graphdb.path')
 
         def graphDir = new File(path)
-        if(graphDir.exists() && c == 'y'){
-            //log.debug("Cleaning the graph directory ${path}")
-            FileUtils.cleanDirectory(graphDir)
-        }
-
         BaseConfiguration conf = new BaseConfiguration()
-        //conf.setProperty("storage.batch-loading", true);
-        //conf.setProperty("autotype", "none");
+
         conf.setProperty("storage.directory", path);
         conf.setProperty("storage.backend", "persistit");
-        conf.setProperty("storage.transactions","false")
-        //conf.setProperty("storage.buffer-size", "1073741824") // 1G
-        //conf.setProperty("buffer.count.16384", "50000");
+
+        if(createGraph){
+            if(graphDir.exists()){
+                FileUtils.cleanDirectory(graphDir)
+            }
+
+            //conf.setProperty("storage.batch-loading", true);
+            //conf.setProperty("autotype", "none");
+            conf.setProperty("storage.transactions", "false")
+            conf.setProperty("storage.buffer-size", "1073741824") // 1G
+            conf.setProperty("buffer.count.16384", "50000");
+        }
         graph = TitanFactory.open(conf)
 
-        if (c == 'y') {
-            //log.info("Populating the graph.")
+
+        if(createGraph){
             graph.makeKey("noun").dataType(String).indexed(Vertex).make()
             graph.commit()
+            log.debug("Populating the graph.")
             GraphService.populateGraph(graph, '/reverb_clueweb_tuples-1.1.triples.clean.csv')
         }
     }
 
     @Test
-    void testGraphLoaded(){
+    void testGraphLoaded() {
         def vertexCnt = graph.vertices.size()
         assertEquals('All vertices were loaded', 2220582, vertexCnt)
     }
 
     @Test
-    void testQueries(){
-        def chargerVertexCnt = graph.V('noun', 'the chargers').count()
-        assertEquals('Able to query by vertex property', 137, chargerVertexCnt)
+    void testQueries() {
+        def whoGotJoeMontana = graph.V('noun', 'Joe Montana').inE('traded for').outV.noun.next()
+        assertEquals('Able to query & traverse the graph', 'the Chiefs', whoGotJoeMontana)
 
-        def whoGotJoeMontana = graph.V('noun', 'joe montana').inE('traded for').outV.noun.next()
-        assertEquals('Able to query & traverse the graph', 'the chiefs', whoGotJoeMontana)
+        def chargerVertexCnt = graph.V('noun', 'the Chargers').count()
+        assertEquals('Able to query by vertex property', 1, chargerVertexCnt)
+
     }
 
     @AfterClass
